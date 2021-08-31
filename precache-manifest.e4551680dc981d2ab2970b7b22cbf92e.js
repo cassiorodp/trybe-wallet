df35a57cfaf33fe89a33aaee933e53dbc7faf487
Cássio Pereira <cassiorodp@gmail.com>
2021-08-31 10:59:00 -0300

    Updates

---
+++ precache-manifest.e4551680dc981d2ab2970b7b22cbf92e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba4588dea794584fb700eafdb09eca6",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "38fc23143443dfac7bc9",
+    "url": "/trybe-wallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7bc3148c51b35abd86b",
+    "url": "/trybe-wallet/static/js/2.221e1bbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.221e1bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fc23143443dfac7bc9",
+    "url": "/trybe-wallet/static/js/main.2883e559.chunk.js"
+  },
+  {
+    "revision": "cf2cc8bb520cf5f8882b",
+    "url": "/trybe-wallet/static/js/runtime-main.ba48076f.js"
+  },
+  {
+    "revision": "1a3bd7320bf2bbc7147569f46ea75b61",
+    "url": "/trybe-wallet/static/media/bitcoin.1a3bd732.gif"
+  }
+]);
